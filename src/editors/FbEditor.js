b9b0cd0fe88a7e6f86155464d90806a3c73ba855
Raudelgar <raudel24@gmail.com>
2020-05-24 20:11:50 -0400

    adding buttons
    this btn will manage the final state of the editor

--- src/editors/FbEditor.js
+++ src/editors/FbEditor.js
@@ -3,6 +3,7 @@ import { Editor, EditorState } from "draft-js";
 import "draft-js/dist/Draft.css";
 import "./editors.scss";
 import { FiEdit } from "react-icons/fi";
+import Buttons from "./buttons/Buttons.js";
 
 export default function FbEditor() {
   const [editorState, setEditorState] = useState(() =>
@@ -11,7 +12,15 @@ export default function FbEditor() {
   const [isEditor, setEditor] = useState(false);
 
   const handleEditArea = () => setEditor(true);
-
+  const handleCancel = () => {
+    console.log("discard changes");
+    setEditorState(() => EditorState.undo(editorState));
+    setEditor(false);
+  };
+  const handleSave = () => {
+    console.log("save changes");
+    setEditor(false);
+  };
   return (
     <div className="editor-content">
       <FiEdit
@@ -25,6 +34,7 @@ export default function FbEditor() {
           placeholder="Write your content here ..."
         />
       </div>
+      {isEditor && <Buttons save={handleSave} cancel={handleCancel} />}
     </div>
   );
 }
